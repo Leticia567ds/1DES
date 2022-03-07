54b17ee19a24044d1f929c4aa3304016088754f9
Leticia567ds <leticiaamarallopes8@gmail.com>
2022-03-07 11:25:41 -0300

    t

--- Fpoo/workspace/Aula 13/Aula 13/src/p/p/Matrizaleatorio.java
+++ Fpoo/workspace/Aula 13/Aula 13/src/p/p/Matrizaleatorio.java
@@ -6,31 +6,30 @@ import java.util.Scanner;
 
 public class Matrizaleatorio {
 	public static Scanner entrada;
-	public static void main(String[] args)throws IOException {
-		entrada = new Scanner(System.in);
+ 
+	public static void main(String[] args)throws IOException {	
 		MatrizAle();
-		
+	 
+	
 	}
 
- public static void MatrizAle(){
-	  
+	public static void MatrizAle() {
+	
+	int [][] gerar = new int[5][5];
 	 Random rand = new Random() ;
-	 int [][] gerar = new int[5][5];
 	  for(int i=0; i < 5; i++) {
-		for(int j = 0; j < 5; j++)  {
+		for(int j = 0; j < 5; j++)  
 			gerar[i][j] = rand.nextInt(100);
-			if(gerar[i][j] % 2 == 0) {
-			 
-				 
-			}
-	System.out.println("gerar[" + i + "][" + j + "]"+ gerar[i][j]);
-	      
-	   
+			
 		}
-		
-		
-	  }
 		 
- }
+		System.out.println("gerar[" + i + "][" + j + "]"+gerar[i][j]);
+	  
+	}
+	
+	}
+	 
+	
 }
+
  
