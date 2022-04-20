ee03754dc919484f435ac3f694bc921b1cb329ae
Leticia567ds <leticiaamarallopes8@gmail.com>
2022-04-20 11:25:47 -0300

    sy

--- Fpoo/workspace/Aula 22/Exercício-002/src/atv3/Arquivo.java
+++ Fpoo/workspace/Aula 22/Exercício-002/src/atv3/Arquivo.java
@@ -8,40 +8,39 @@ import java.util.ArrayList;
 
 public class Arquivo {
 
-	public void Escrever(String info, String fileName, boolean append) {
+	public void escrever(ArrayList<Integer> info, String fileName, boolean append) {
 		try {
-			FileWriter fw = new FileWriter(fileName + ".txt", append);
+			FileWriter fw = new FileWriter("Numeros.txt", true);
 			BufferedWriter bw = new BufferedWriter(fw);
 
-			bw.write(info + "\r\n");
+			for (Integer in : info) {
+				bw.write( in+"\r\n");
+			}
 
 			bw.close();
 			fw.close();
 		} catch (Exception e) {
-			System.out.println();
+			System.out.println(e.toString());
 		}
-
 	}
-	public ArrayList<Integer> ler(String fileName) {
-		ArrayList<Integer> arquivo = new ArrayList<>();
-		try {
-			FileReader fr = new FileReader(fileName + ".txt");
-			BufferedReader br = new BufferedReader(fr);
-			String linha = ";";
-			while ((linha = br.readLine()) != null) {
-				arquivo.add(linha);
-			}
 
+	public ArrayList<Integer> ler(String FileName) {
+		ArrayList<Integer> list = new ArrayList<>();
+		try {
+			FileReader fr = new FileReader("Numeros.txt");
+			BufferedReader br = new BufferedReader(fr);
+
+			
+			String linh = "";
+			while ((linh = br.readLine()) != null) {
+				 list.add(Integer.parseInt(linh));
+			}
 			br.close();
 			fr.close();
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
-
-		return arquivo;
+		return list;
 	}
 
 }
-
-
-
--- Fpoo/workspace/Aula 22/Exercício-002/src/atv3/Main.java
+++ Fpoo/workspace/Aula 22/Exercício-002/src/atv3/Main.java
@@ -3,46 +3,57 @@
 
 package atv3;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-import atv2.Arquivo;
+import atv3.Arquivo;
 
 public class Main {
-
+      private static Arquivo arq = new Arquivo();
+      private static Scanner input = new Scanner(System.in);
+      private static Random r = new Random();
 	public static void main(String[] args) {
-		 int[]numeros = new int[10];
-		Scanner input = new Scanner(System.in);
-		Arquivo arq = new Arquivo();
-		int opcao = 0;
-		do {
-			System.out.println("1 - Gerar N.Aleatórios");
-			System.out.println("2 - Listar");
-			System.out.println("3 - Sair");
-			opcao = input.nextInt();
+		int menu = 0;
+		while(menu != 3) {
+			System.out.println("1.Gerar\n2.Listar em ordem Crescente\n3.Sair");
+			menu= input.nextInt();
 			input.nextLine();
-			switch (opcao) {
+			switch(menu) {
 			case 1:
-				Random rand = new Random();
-            
-				for (int i = 0; i < 10; i++) {
-					numeros[i] = rand.nextInt(100);
-				
-				
-				 
-				arq.Escrever(String.valueOf(numeros[i]), "aleatorios", true);
-				}
+				gerarArquivo();
 				break;
 			case 2:
-				
-				
-				
+				listarNumeros()
+;				break;
+			case 3:
+				System.out.println("Bye!*-*");
 				break;
+				default:
+					System.out.println("erro, opção invalida");
+					break;
 			}
-		} while (opcao != 0);
+		}
 	}
-
+	private static void gerarArquivo() {
+		 
+		ArrayList<Integer> numb = new ArrayList<Integer>(); 
+		for (int i = 0; i < 10; i++) {
+			numb.add(r.nextInt(100)); 
+		}
+		
+		arq.escrever(numb, "Numeros", false);
+		System.out.println("Arquivo gerado com sucesso");
+	}
+	
+	private static void listarNumeros() {
+		ArrayList<Integer> dads = arq.ler("Numeros");
+		
+		dads.sort(null);
+		
+		for (Integer num : dads) {
+			System.out.println(num);
+		}
+}
 }
