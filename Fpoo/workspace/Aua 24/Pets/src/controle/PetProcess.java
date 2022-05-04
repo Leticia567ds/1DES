fee519fe80164791be080a1ab9e63d270eafd245
Leticia567ds <leticiaamarallopes8@gmail.com>
2022-05-04 11:21:36 -0300

    s

--- Fpoo/workspace/Aua 24/Pets/src/controle/PetProcess.java
+++ Fpoo/workspace/Aua 24/Pets/src/controle/PetProcess.java
@@ -10,12 +10,13 @@ public class PetProcess {
 	public static ArrayList<Pet> pets = new ArrayList<>();
 	
 	public static void carregarTestes() throws ParseException {
-		//Limpar a lista
+
 		pets = new ArrayList<>();
-		//Prencher com teste
-		pets.add(new Pet(1,"Gato","mimy","Angorá",30,"02/01/2018","Marta","(19)9986-7543"));
-		pets.add(new Pet(2,"Cachorro","Toto","Vira Lata",30,"02/01/2017","João","(19)9986-7543"));
-		pets.add(new Pet(3,"Passaro","yudy","Curió",20,"02/01/2022","Susy","(19)9986-7543"));
-		pets.add(new Pet(4,"Coelho","ki","American",30,"02/01/2017","ly","(19)9986-7543"));                                                           
+
+		pets.add(new Pet(0,"Gato","mimy","Angorá",30,"02/01/2018","Marta","(19)9986-7543"));
+		pets.add(new Pet(1,"Cachorro","Toto","Vira Lata",30,"02/01/2017","João","(19)9986-7543"));
+		pets.add(new Pet(2,"Passaro","yudy","Curió",20,"02/01/2022","Susy","(19)9986-7543"));
+		pets.add(new Pet(3,"Coelho","ki","American",30,"02/01/2017","ly","(19)9986-7543"));    
+		 
 	}
 }
--- Fpoo/workspace/Aua 24/Pets/src/visao/PetsForm.java
+++ Fpoo/workspace/Aua 24/Pets/src/visao/PetsForm.java
@@ -18,209 +18,265 @@ import javax.swing.JTextField;
 
 import controle.PetProcess;
 import modelo.Pet;
- 
 
-public class PetsForm extends JFrame implements ActionListener{  
+public class PetsForm extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	
 	private JPanel painel;
-	private JLabel id, especie, nome, raca, peso, nas, nomeDono, telefone, rotulos,imagem;
-    
-	private JTextField tfid,tfnome, tfraca, tfpeso, tfnas, tfnomeDono, tftelefone;
-	private JComboBox<String> Especie;
-	private JTextArea Tela;
-	private String[] imagens =  {  "C:\\Users\\des\\Desktop\\1DES\\Fpoo\\workspace\\Aua 24\\Pets\\Imagem\\cat.jpg",
-			"C:\\Users\\des\\Desktop\\1DES\\Fpoo\\workspace\\Aua 24\\Pets\\Imagem\\22.jpg", 
-			"C:\\Users\\des\\Desktop\\1DES\\Fpoo\\workspace\\Aua 24\\Pets\\Imagem\\Pássaros.jpg",
-			"C:\\Users\\des\\Desktop\\1DES\\Fpoo\\workspace\\Aua 24\\Pets\\Imagem\\coelho.jpg",
-			"Outros"
-	};
-	private ImageIcon icon;
+	private JLabel id, especie, nomePet, raca, peso, nascimento, nomeDono, telefone, rotulos, imagem;
+	private JTextField tfId, tfNomePet, tfRaca, tfPeso, tfNascimento, tfNomeDono, tfTelefone;
+	private JComboBox<String> cbEspecie;
+	private JTextArea verResultados;
 	private JButton create, read, update, delete;
+	private String imgIco = "./assets/icone.png";
+	private String[] imagens = { "C:\\Users\\des\\Desktop\\1DES\\Fpoo\\workspace\\Aua 24\\Pets\\Imagem\\cat.jpg",
+			"C:\\Users\\des\\Desktop\\1DES\\Fpoo\\workspace\\Aua 24\\Pets\\Imagem\\22.jpg",
+			"C:\\Users\\des\\Desktop\\1DES\\Fpoo\\workspace\\Aua 24\\Pets\\Imagem\\PÃ¡ssaros.jpg",
+			"C:\\Users\\des\\Desktop\\1DES\\Fpoo\\workspace\\Aua 24\\Pets\\Imagem\\coelho.jpg",
+			"C:\\Users\\des\\Desktop\\1DES\\Fpoo\\workspace\\Aua 24\\Pets\\Imagem\\Joia.jpg" };
+	private ImageIcon icon;
 	private int autoId = PetProcess.pets.size() + 1;
 	private String texto = "";
-	
-	
-	PetsForm() throws ParseException {
-		setTitle("Formulário de Pets");
-		setBounds(200, 300, 500, 500);
+
+	PetsForm() {
+		setTitle("Formulario de Pets");
+		setBounds(200, 500, 700, 500);
+		setIconImage(new ImageIcon(imgIco).getImage());
 		painel = new JPanel();
-		painel.setBackground(new Color(255, 190 , 203 ));
+		painel.setBackground(new Color(255, 191, 203));
 		setContentPane(painel);
-		 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(null);
-		
+
 		id = new JLabel("Id:");
 		id.setBounds(20, 20, 120, 30);
 		painel.add(id);
-		
-		especie = new JLabel("especie:");
+		especie = new JLabel("Especie:");
 		especie.setBounds(20, 50, 120, 30);
 		painel.add(especie);
-		
-		nome = new JLabel("Nome:");
-		nome.setBounds(20,80, 120, 30);
-		painel.add(nome);
-		
-		raca = new JLabel("Raça:");
+		nomePet = new JLabel("Nome pet:");
+		nomePet.setBounds(20, 80, 120, 30);
+		painel.add(nomePet);
+		raca = new JLabel("Raï¿½a:");
 		raca.setBounds(20, 110, 120, 30);
 		painel.add(raca);
-		
 		peso = new JLabel("Peso:");
-		peso.setBounds(20,137, 120, 30);
+		peso.setBounds(20, 140, 120, 30);
 		painel.add(peso);
-		
-		nas = new JLabel("Nascimento:");
-		nas.setBounds(20,166, 120, 30);
-		painel.add(nas);
-		
-		nomeDono = new JLabel("Dono:");
+		nascimento = new JLabel("Nascimento:");
+		nascimento.setBounds(20, 170, 120, 30);
+		painel.add(nascimento);
+		nomeDono = new JLabel("Nome dono:");
 		nomeDono.setBounds(20, 200, 120, 30);
 		painel.add(nomeDono);
-		
 		telefone = new JLabel("Telefone:");
-		telefone.setBounds(20,230, 120, 30);
+		telefone.setBounds(20, 230, 120, 30);
 		painel.add(telefone);
-		
-		rotulos = new JLabel("Id:     Espécie:       Pet:      Raça:      Peso:       Idade:       Dono:        Telefone:");
-		rotulos.setBounds(20, 270, 400, 30);
+		rotulos = new JLabel(" Id | Especie | NomePet | RaÃ§a | Peso |Idade |Dono | Telefone:");
+		rotulos.setBounds(20, 270, 500, 30);
 		painel.add(rotulos);
-		
-		tfid = new JTextField(String.format("%d", autoId));
-		tfid.setEnabled(true);
-		tfid.setBackground(new Color(210, 210 , 210));
-		tfid.setBounds(100, 20, 120, 30);
-	    painel.add(tfid);
-	    
-	    Especie = new JComboBox(new String[] { "Gato", "Cachorro", "passaro", "Coelho", "Outro" });
-		Especie.setBounds(100, 50, 120, 30);
-		painel.add(Especie);
-		
-	    tfnome = new JTextField();
-		tfnome.setBounds(100, 80, 120, 30);
-	    painel.add(tfnome);
-	    
-	    tfraca = new JTextField();
-		tfraca.setBounds(100, 110, 120, 30);
-	    painel.add(tfraca);
-	    
-	    tfpeso = new JTextField();
-		tfpeso.setBounds(100, 140, 120, 30);
-	    painel.add(tfpeso);
-	    
-	    tfnas = new JTextField();
-		tfnas.setBounds(100, 170, 120, 30);
-	    painel.add(tfnas);
-	    
-	    tfnomeDono = new JTextField();
-		tfnomeDono.setBounds(100,200, 120, 30);
-	    painel.add(tfnomeDono);
-	    
-	    tftelefone = new JTextField();
-		tftelefone.setBounds(100,230, 120, 30);
-	    painel.add(tftelefone);
-	    
-	    Tela = new JTextArea();
-		Tela.setEnabled(true);
-		Tela.setBounds(20,300, 400, 150);
-		Tela.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.red));
+
+		tfId = new JTextField(String.format("%d", autoId));
+		tfId.setEditable(false);
+		tfId.setBounds(140, 20, 260, 30);
+		painel.add(tfId);
+		cbEspecie = new JComboBox<String>(new String[] { "Gato", "Cachorro", "Passaro", "Coelho", "Outro" });
+		cbEspecie.setBounds(140, 50, 260, 30);
+		painel.add(cbEspecie);
+		tfNomePet = new JTextField();
+		tfNomePet.setBounds(140, 80, 260, 30);
+		painel.add(tfNomePet);
+		tfRaca = new JTextField();
+		tfRaca.setBounds(140, 110, 260, 30);
+		painel.add(tfRaca);
+		tfPeso = new JTextField();
+		tfPeso.setBounds(140, 140, 260, 30);
+		painel.add(tfPeso);
+		tfNascimento = new JTextField();
+		tfNascimento.setBounds(140, 170, 260, 30);
+		painel.add(tfNascimento);
+		tfNomeDono = new JTextField();
+		tfNomeDono.setBounds(140, 200, 260, 30);
+		painel.add(tfNomeDono);
+		tfTelefone = new JTextField();
+		tfTelefone.setBounds(140, 230, 260, 30);
+		painel.add(tfTelefone);
+		verResultados = new JTextArea();
+		// verResultados.setEnabled(false);
+		verResultados.setBounds(20, 300, 660, 150);
+		verResultados.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.red));
 		preencherAreaDeTexto();
-		painel.add(Tela);
-		 
-		
-		
+		painel.add(verResultados);
 		imagem = new JLabel();
-		imagem.setBounds(250,150,215,120);
+		imagem.setBounds(410, 145, 150, 115);
+		imagem.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.red));
 		alternarImagens(0);
 		painel.add(imagem);
-		
-		imagem.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.red));
-		
+
 		create = new JButton("Cadastrar");
 		read = new JButton("Buscar");
 		update = new JButton("Atualizar");
 		delete = new JButton("Excluir");
-		create.setBounds(280, 20, 150, 30);
-		read.setBounds(280, 50, 150, 30);
-		update.setBounds(280, 80, 150, 30);
-		delete.setBounds(280, 110, 150, 30);
+		create.setBounds(410, 20, 150, 30);
+		read.setBounds(410, 50, 150, 30);
+		update.setBounds(410, 80, 150, 30);
+		delete.setBounds(410, 110, 150, 30);
 		update.setEnabled(false);
 		delete.setEnabled(false);
 		painel.add(create);
 		painel.add(read);
 		painel.add(update);
 		painel.add(delete);
-		
-		
-		Especie.addActionListener(this);
+
+		// Ouvir os eventos dos Botï¿½es, ComboBox e outros
+		cbEspecie.addActionListener(this);
 		create.addActionListener(this);
 		read.addActionListener(this);
 		update.addActionListener(this);
 		delete.addActionListener(this);
-		
-		
+
 	}
-	
+
 	private void alternarImagens(int indice) {
-		icon = new ImageIcon(new ImageIcon(imagens[indice]).getImage().getScaledInstance(212,180, 100));
+		icon = new ImageIcon(new ImageIcon(imagens[indice]).getImage().getScaledInstance(150, 115, 100));
 		imagem.setIcon(icon);
 	}
-	
-	 
-	//CREATE - CRUD
+
+	// CREATE - CRUD
 	private void cadastrar() throws NumberFormatException, ParseException {
-		if (tfnome.getText().length() != 0 && tfraca.getText().length() != 0 && tfpeso.getText().length() != 0
-				&& tfnas.getText().length() != 0 && tfnomeDono.getText().length() != 0
-				&& tftelefone.getText().length() != 0) {	
-			PetProcess.pets
-					.add(new Pet(autoId, Especie.getSelectedItem().toString(), tfnome.getText(), tfraca.getText(), Float.parseFloat(tfpeso.getText()),tfnas.getText(), tfnomeDono.getText(), tftelefone.getText()));
-		autoId++;
+		if (tfNomePet.getText().length() != 0 && tfRaca.getText().length() != 0 && tfPeso.getText().length() != 0
+				&& tfNascimento.getText().length() != 0 && tfNomeDono.getText().length() != 0
+				&& tfTelefone.getText().length() != 0) {
+			PetProcess.pets.add(new Pet(autoId, cbEspecie.getSelectedItem().toString(), tfNomePet.getText(),
+					tfRaca.getText(), Float.parseFloat(tfPeso.getText()), tfNascimento.getText(), tfNomeDono.getText(),
+					tfTelefone.getText()));
+			autoId++;
 			preencherAreaDeTexto();
 			limparCampos();
 		} else {
 			JOptionPane.showMessageDialog(this, "Favor preencher todos os campos.");
 		}
 	}
-	
-	private void limparCampos(){
-		tfnome.setText(null);
-		tfraca.setText(null);
-		tfpeso.setText(null);
-		tfnas.setText(null);
-		tfnomeDono.setText(null);
-		tftelefone.setText(null);
+
+	private void limparCampos() {
+		tfNomePet.setText(null);
+		tfRaca.setText(null);
+		tfPeso.setText(null);
+		tfNascimento.setText(null);
+		tfNomeDono.setText(null);
+		tfTelefone.setText(null);
 	}
-	
+
 	private void preencherAreaDeTexto() {
-		texto = ""; 
+		texto = ""; // Limpar a ï¿½rea de texto antes de preenher
 		for (Pet p : PetProcess.pets) {
 			texto += p.toString();
 		}
-		Tela.setText(texto);
+		verResultados.setText(texto);
 	}
-	
+
+	int obterIndiceEspecie(String especie) {
+		switch (especie) {
+		case "Gato":
+			return 0;
+		case "Cachorro":
+			return 1;
+		case "Passaro":
+			return 2;
+		case "Coelho":
+			return 3;
+		case "Outros":
+			return 4;
+		default:
+			return -1;
+		}
+
+	}
+
+	private void buscar() {
+		String entrada = JOptionPane.showInputDialog("Digite o Id do animal:");
+		int id = Integer.parseInt(entrada);
+		Pet pet = new Pet(id);
+		if (PetProcess.pets.contains(pet)) {
+
+			int indice = PetProcess.pets.indexOf(pet);
+			tfId.setText(PetProcess.pets.get(indice).getid("s"));
+			cbEspecie.setSelectedIndex(obterIndiceEspecie(PetProcess.pets.get(indice).getEspecie()));
+			tfNomePet.setText(PetProcess.pets.get(indice).getNome());
+			tfRaca.setText(PetProcess.pets.get(indice).getRaca());
+			tfPeso.setText(PetProcess.pets.get(indice).getPeso("s"));
+			tfNascimento.setText(PetProcess.pets.get(indice).getNas("s"));
+			tfNomeDono.setText(PetProcess.pets.get(indice).getNomeDono());
+			tfTelefone.setText(PetProcess.pets.get(indice).getTelefone());
+			create.setEnabled(false);
+			read.setEnabled(true);
+			update.setEnabled(true);
+			delete.setEnabled(true);
+		} else {
+			JOptionPane.showMessageDialog(this, "Pet nÃ£o encontrado");
+		}
+
+	}
+
+	private void alterar() throws NumberFormatException, ParseException {
+		int id = Integer.parseInt(tfId.getText());
+		Pet pet = new Pet();
+		int indice = PetProcess.pets.indexOf(pet);
+		PetProcess.pets.remove(indice);
+		if (tfNomePet.getText().length() != 0 && tfRaca.getText().length() != 0 && tfPeso.getText().length() != 0
+				&& tfNascimento.getText().length() != 0 && tfNomeDono.getText().length() != 0
+				&& tfTelefone.getText().length() != 0) {
+			PetProcess.pets.set(indice,new Pet(id, cbEspecie.getSelectedItem().toString(), tfNomePet.getText(),
+					tfRaca.getText(), Float.parseFloat(tfPeso.getText()), tfNascimento.getText(), tfNomeDono.getText(),
+					tfTelefone.getText()));
+			preencherAreaDeTexto();
+			limparCampos();
+		} else {
+			JOptionPane.showMessageDialog(this, "Favor preencher todos os campos.");
+		}
+		create.setEnabled(true);
+		update.setEnabled(false);
+		delete.setEnabled(false);
+		 
+	}
+
+	private void deletar() {
+		int id = Integer.parseInt(tfId.getText());
+		Pet pet = new Pet();
+		int indice = PetProcess.pets.indexOf(pet);
+		PetProcess.pets.remove(indice);
+		limparCampos();
+		preencherAreaDeTexto();
+		delete.setEnabled(false);
+		tfId.setText(String.format("%d", autoId));
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == Especie) {
-			alternarImagens(Especie.getSelectedIndex());
+		if (e.getSource() == cbEspecie) {
+			alternarImagens(cbEspecie.getSelectedIndex());
 		}
 		if (e.getSource() == create) {
 			try {
 				cadastrar();
 			} catch (NumberFormatException | ParseException e1) {
-				System.out.println("");
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
 		}
 		if (e.getSource() == read) {
-
+			buscar();
 		}
 		if (e.getSource() == update) {
-
+			try {
+				alterar();
+			} catch (NumberFormatException | ParseException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
 		if (e.getSource() == delete) {
-
+			deletar();
 		}
 	}
 
